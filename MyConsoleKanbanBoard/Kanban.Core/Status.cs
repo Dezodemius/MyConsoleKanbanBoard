a1a9ffabe0b6428aad6a6c20a0dc4feea9284069
Гладков Егор <gladkovyegor@gmail.com>
2026-05-06 19:58:37 +0400

    Create Status.cs

---
+++ MyConsoleKanbanBoard/Kanban.Core/Status.cs
@@ -0,0 +1,22 @@
+﻿namespace Kanban.Core;
+
+/// <summary>
+/// Статусы задач.
+/// </summary>
+public enum Status
+{
+    /// <summary>
+    /// Можно взят в работу.
+    /// </summary>
+    ToDo,
+    
+    /// <summary>
+    /// В работе.
+    /// </summary>
+    InProgress,
+    
+    /// <summary>
+    /// Выполнено.
+    /// </summary>
+    Done
+}
